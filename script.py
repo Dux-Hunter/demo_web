5af9014d4406852c1232c7c471646a356b92a890
Dux-Hunter <ulquiorra1883@gmail.com>
2025-05-20 02:26:43 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,5 +2,6 @@ import streamlit as stl
 stl.header("My website")
 stl.header('my first python script')
 stl.subheader('Subheadding is edited now and i want to see it working again')
+stl.subheader('ashish gupta.com')
 
 
